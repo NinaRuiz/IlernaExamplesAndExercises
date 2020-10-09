7ed1044f6be35ba606f889d2fa2c1f7a045ebb0c
NinaRuiz <nina.ruizandres@gmail.com>
2020-10-09 14:05:07 +0200

    Made menu and Example 1

---
+++ CodeExamples/Example1.cs
@@ -0,0 +1,55 @@
+using System;
+using ConsoleApp3;
+
+namespace ConsoleApp3.CodeExamples
+{
+    public class Example1
+    {
+        // Example page to show it on console.
+        public static string ExamplePage = "p. 16";
+
+        public static void Run ()
+        {
+            // Shows example page on console
+            Console.WriteLine("> Example on: " + ExamplePage);
+            // Callback to the Example method
+            Example();
+            // Callback to the End method
+            End();
+        }
+        
+        /**
+         * Example method
+         * It shows on console the lines "Hello World!" and "Programming..."
+         */
+        public static void Example()
+        {
+            // Shows on console "Hello World!" and then jump to the next line.
+            Console.WriteLine("Hello World!");
+            // Shows on console "Programing...", it doesn't jump to next line, if we write another 
+            // thing on console, it will continue the text on the same line.
+            Console.Write("Programming...");
+            
+        }
+        
+        /**
+         *  End method
+         *  Return the user to the menu method.
+         */
+        public static void End()
+        {
+            Console.WriteLine("\nPress a enter to continue...");
+            var key = Console.ReadLine();
+            if (key == null)
+            {
+                Console.Clear();
+                Program.Menu();
+                return;
+            }
+            // It returns to the menu method i made on Program.cs file
+            // Callback to the menu method on Program.cs
+            Console.Clear();
+            Program.Menu();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
+using ConsoleApp3;
+using ConsoleApp3.CodeExamples;
 
 namespace ConsoleApp3
 {
     class Program
     {
+        private static List<string> Examples = new List<string>();
+        private static int _examplesNum = 0;
+
         static void Main(string[] args)
+        {    
+            CreateExamplesList();
+            Menu();
+        }    
+
+        public static void Menu()
         {
-            Console.WriteLine("Hello World!");
-            Console.Write("Programing...");
+            Console.WriteLine("> Menu: ");
+            foreach (var example in Examples)
+            {
+                Console.WriteLine(example);
+            }
+            Console.WriteLine("Write the number of the example you want to see:");
+
+            var input = Console.ReadLine();
+            var select = 0;
+            if (input == null)
+            {
+                Console.WriteLine("You must choose.");
+                Menu();
+                return;
+            }
+            
+            select = Int32.Parse(input);
+            
+            switch (select)
+            {
+                case 0:
+                    break;
+                case 1:
+                    Console.Clear();
+                    Example1.Run();
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("That example doesn't exist.");
+                    Menu();
+                    break;
+            }
+        }
+
+        static void CreateExamplesList()
+        {
+            AddExample(Example1.ExamplePage);
+            Examples.Add("0 Exit");
+        }
+
+        private static void AddExample(string page)
+        {
+            _examplesNum++;
+            Examples.Add(_examplesNum + " Example " + page);
         }
     }
 }
